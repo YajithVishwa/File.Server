c74e819535c33a58823f4e3121521129c0384d25
YajithVishwa <yajithvishwa2001@gmail.com>
2024-10-27 00:28:33 +0530

    docker file changes

--- config.py
+++ config.py
@@ -3,6 +3,4 @@ import os
 
 class Config:
     DEBUG = True
-    USERNAME = os.environ.get('USERNAME') or 'test.local'
-    PASSWORD = os.environ.get('PASSWORD') or 'test123'
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or 'D:/Workspace/Python Project/File.Server/app/upload'
